3e9f3b3d19680d2062591aa2bfd13667286abb31
Sartajkhan228 <khanalam7328@gmail.com>
2025-12-06 01:33:51 +0500

    first sever commit in ecommerce app

---
+++ dist/models/user.d.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+interface IUser extends Document {
+    _id: string;
+    name: string;
+    email: string;
+    photo: string;
+    role: "admin" | "user";
+    gender: "male" | "female";
+    dob: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    age: number;
+}
+export declare const User: mongoose.Model<IUser, {}, {}, {}, mongoose.Document<unknown, {}, IUser, {}, mongoose.DefaultSchemaOptions> & IUser & Required<{
+    _id: string;
+}> & {
+    __v: number;
+}, any, IUser>;
+export {};
+//# sourceMappingURL=user.d.ts.map
---
+++ dist/models/user.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import validator from "validator";
+const schema = new mongoose.Schema({
+    _id: { String, required: [true, "Please inter ID"] },
+    name: { String, required: [true, "Please inter name"] },
+    email: {
+        String, unique: [true, "Email already exists"],
+        required: [true, "Please inter email"],
+        validate: validator.default.isEmail,
+    },
+    photo: { String, required: [true, "Please add photo"] },
+    role: { String, enum: ["admin", "user"], default: "user", },
+    gender: { String, enum: ["male", "female"], required: [true, "Please inter Gender"] },
+    dob: { Date, required: [true, "Please inter Date of birth"] },
+}, {
+    timestamps: true
+});
+schema.virtual("age").get(function () {
+    const today = new Date();
+    const dob = this.dob;
+    let age = today.getFullYear() - dob.getFullYear();
+    if (today.getMonth() < dob.getMonth() || today.getMonth() === dob.getMonth()
+        && today.getDate() < dob.getDate()) {
+        age--;
+    }
+    return age;
+});
+export const User = mongoose.model("User", schema);
+//# sourceMappingURL=user.js.map
